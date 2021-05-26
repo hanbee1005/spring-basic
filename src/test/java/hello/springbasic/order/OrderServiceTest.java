82adbdefe5b9988a7d64f89f32a0f16cd1cad4fc
hanbee1005 <sonhanbi1002@gmail.com>
2021-05-26 16:09:56 +0900

    AppConfig로 구현 클래스를 선택하는 의존성 주입 구현

--- src/test/java/hello/springbasic/order/OrderServiceTest.java
+++ src/test/java/hello/springbasic/order/OrderServiceTest.java
@@ -1,13 +1,22 @@
 package hello.springbasic.order;
 
+import hello.springbasic.AppConfig;
 import hello.springbasic.member.*;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     public void createOrder() {
