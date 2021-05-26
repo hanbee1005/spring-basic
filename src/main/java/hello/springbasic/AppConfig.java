d7fc45cf5af523fd9e3f30a009e8ce78f4a91237
hanbee1005 <sonhanbi1002@gmail.com>
2021-05-26 17:07:20 +0900

    스프링 컨테이너를 사용하도록 수정 및 확인

--- src/main/java/hello/springbasic/AppConfig.java
+++ src/main/java/hello/springbasic/AppConfig.java
@@ -8,21 +8,28 @@ import hello.springbasic.member.MemberServiceImpl;
 import hello.springbasic.member.MemoryMemberRepository;
 import hello.springbasic.order.OrderService;
 import hello.springbasic.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
---
+++ src/main/java/hello/springbasic/MemberApp.java
@@ -0,0 +1,21 @@
+package hello.springbasic;
+
+import hello.springbasic.member.Grade;
+import hello.springbasic.member.Member;
+import hello.springbasic.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
+    }
+}
