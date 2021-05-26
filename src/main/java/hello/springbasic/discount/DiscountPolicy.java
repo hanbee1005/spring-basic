3b6c276a00ced63251c341f7fbfa0e99a99ae5a4
hanbee1005 <sonhanbi1002@gmail.com>
2021-05-26 14:42:09 +0900

    할인 정책 인터페이스 및 고정 할인 정책 구현체 생생, 구현

---
+++ src/main/java/hello/springbasic/discount/DiscountPolicy.java
@@ -0,0 +1,13 @@
+package hello.springbasic.discount;
+
+import hello.springbasic.member.Member;
+
+public interface DiscountPolicy {
+
+    /**
+     * @param member
+     * @param price
+     * @return 할인 대상 금액
+     */
+    int discount(Member member, int price);
+}
