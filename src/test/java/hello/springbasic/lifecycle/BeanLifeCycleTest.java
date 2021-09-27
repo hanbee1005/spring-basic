9911056d6d01fe8ac1f4a212dc716031504f4ab9
hanbee <sonhanbi1002@gmail.com>
2021-09-28 00:14:07 +0900

    spring bean life cycle test 객체 생성

---
+++ src/test/java/hello/springbasic/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,28 @@
+package hello.springbasic.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    void lifeCycleTest() {
+        // AnnotationConfigApplicationContext ac 또는
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LiftCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();  // 기본 ApplicationContext 는 제공 x
+    }
+
+    @Configuration
+    static class LiftCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
