8f2bf0f51d61bcb06e6fa8ba046d8a5d6b40b677
hanbee <sonhanbi1002@gmail.com>
2021-09-28 11:11:27 +0900

    ObjectProvider를 사용하여 PrototypeBean을 사용할 떄마다 새로 생성

--- src/test/java/hello/springbasic/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/springbasic/scope/SingletonWithPrototypeTest1.java
@@ -1,6 +1,8 @@
 package hello.springbasic.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -35,18 +37,18 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;  // 생성 시점에 주입
 
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        // ObjectFactory 를 써도 됨 - getObject() 하나만 제공
+        // SpringContainer 에서 조회를 대신 해주는 역할
+        @Autowired ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
