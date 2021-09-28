7fa91274cdc9e4853c793c513061ff7b482e62a0
hanbee <sonhanbi1002@gmail.com>
2021-09-28 10:13:04 +0900

    prototypeBean 확인

--- src/test/java/hello/springbasic/scope/SingletonTest.java
+++ src/test/java/hello/springbasic/scope/SingletonTest.java
@@ -1,7 +1,6 @@
 package hello.springbasic.scope;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
